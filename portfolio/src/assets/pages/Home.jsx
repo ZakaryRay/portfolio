1980a64e4895c4c43204bf961d53d87c9520a5dc
Zakary Raymond <zakary-raymond@live.fr>
2026-07-12 16:20:36 -0400

    fixed link

--- portfolio/src/assets/pages/Home.jsx
+++ portfolio/src/assets/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <>
             <Hero />
             <main className="text-light-gray p-10 custom:flex custom:space-x-20 ">
-                <div className="custom:w-1/2">
+                <div className="custom:w-1/2 ">
                     <div className="space-y-3 text-base/7  ">
                         <p className="">
                             Je suis fraichement diplomé, mais completement fou crazy Lorem ipsum
@@ -23,28 +23,33 @@ const Home = () => {
                             fugiat.
                         </p>
                     </div>
-                    <div className="flex flex-col space-y-2 text-gray-purple font-mono custom:w-1/2">
+                    <div className="flex flex-col space-y-2 text-gray-purple font-mono custom:w-1/2 my-10">
                         <a
                             href="mailto:zkary-raymond@live.fr"
-                            className="flex items-center space-x-2 hover:text-white/80 custom:w-fit">
+                            className="group flex items-center space-x-2 hover:text-white/80 custom:w-fit">
                             <i className="bx bxs-envelope"></i>
                             <span>zakary-raymond@live.fr</span>
+                            <i className="bx bxs-chevron-up rotate-45 opacity-0 group-hover:opacity-100 transition-opacity"></i>
                         </a>
+
                         <a
                             href="https://github.com/ZakaryRay"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="flex items-center space-x-2 hover:text-white/80 w-fit">
+                            className="group flex items-center space-x-2 hover:text-white/80 w-fit">
                             <i className="bx bxl-github"></i>
                             <span>github.com/ZakaryRay</span>
+                            <i className="bx bxs-chevron-up rotate-45 opacity-0 group-hover:opacity-100 transition-opacity"></i>
                         </a>
+
                         <a
                             href="https://www.linkedin.com/in/zakaryray/"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="flex items-center space-x-2 hover:text-white/80 w-fit">
+                            className="group flex items-center space-x-2 hover:text-white/80 w-fit">
                             <i className="bx bxl-linkedin"></i>
                             <span>linkedin.com/in/zakaryray</span>
+                            <i className="bx bxs-chevron-up rotate-45 opacity-0 group-hover:opacity-100 transition-opacity"></i>
                         </a>
                     </div>
                 </div>
